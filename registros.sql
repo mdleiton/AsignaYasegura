a449f5b6432e3dce56915218228f63bcb944f296
Lenin Castillo <lehocast@espol.edu.ec>
2017-09-21 22:49:08 -0500

    ingreso datos distrito

---
+++ registros.sql
@@ -0,0 +1,48 @@
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c1','ximena 1','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c2','ximena 2','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c3','Antigua direccion provincial de educacion','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c4','Portete','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c5','Tarqui','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c6','Bellavista-mapasingue','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c7','Vergeles','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c8','Monte siani','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c9','Chongon','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c10','Gomez rendon','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c23','Samborondon','8');
+INSERT INTO distrito( codigo, nombre, zona )
+VALUES ( 'c24','Duran','8');
+INSERT INTO OfertaAcademica( nombre)
+VALUES ( 'Bachillerato General Unificado');
+INSERT INTO OfertaAcademica( nombre)
+VALUES ( 'Bachillerato Internacional');
+INSERT INTO OfertaAcademica( nombre)
+VALUES ( 'Bachillerato Tecnico');
+INSERT INTO OfertaAcademica( nombre, codigo)
+VALUES ( 'Produccion agropcuaria','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ('Conservacion y manejo de recursos naturales','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Industrialización de productos alimenticios','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Promotor en recreación y deporte','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Electromecánica automotriz','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Chapisteria y pintura','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Climatización','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Electrónica de consumo','3');
+INSERT INTO OfertaAcademica( nombre,codigo)
+VALUES ( 'Aplicacion de proyectos de produccion','3');
