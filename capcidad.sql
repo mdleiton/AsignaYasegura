4eb54eb4dc40fc504aca0b69412adbcec8174388
Lenin Castillo <lehocast@espol.edu.ec>
2017-09-28 23:39:17 -0500

    se agrego las asignaciones

---
+++ capcidad.sql
@@ -0,0 +1,8 @@
+INSERT INTO capacidad( capacidad, nivel de estudio)
+VALUES( 45,'Básica superior');
+INSERT INTO capacidad( capacidad, nivel de estudio)
+VALUES( 35,'Básica media');
+INSERT INTO capacidad( capacidad, nivel de estudio)
+VALUES( 30,'Básica elemental');
+INSERT INTO capacidad( capacidad, nivel de estudio)
+VALUES( 25,'Preparatoria');
