02e93360ea69bbe464f2d5c5e093ef6b3498eb0c
mdleiton <mau_leiton96@hotmail.com>
2018-01-19 00:45:57 -0500

    cambio de version de django . de version 1.8 a 2.0

--- AA/settings.py
+++ AA/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
+   'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
